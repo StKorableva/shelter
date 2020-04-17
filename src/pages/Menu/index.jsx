8392db13e2a87bff22821aba05f6691de3a2023d
Anastasiia Vasileva <Anastasiia_Vasileva@epam.com>
2020-04-17 20:04:33 +0300

    added menu, menuItem const, images

--- src/pages/Menu/index.jsx
+++ src/pages/Menu/index.jsx
@@ -7,6 +7,12 @@ export default function Menu() {
     return (
         <div className ='menu'>
             <div className='menu__circle'/>
+            <div className='menu__number1'>01</div>
+            <div className='menu__number2'>02</div>
+            <div className='menu__number3'>03</div>
+            <span>Cats</span>
+            <span>Dogs</span>
+            <span>About</span>
         </div>
 );
 }
