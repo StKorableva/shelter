77c4374fc89c6a4f40ccaefd60033be4dcba426b
Nikita Beliaev <nikita_beliaev@epam.com>
2020-04-30 15:58:49 +0300

    fixed angle detection formula

--- src/components/menuItem/index.jsx
+++ src/components/menuItem/index.jsx
@@ -3,32 +3,29 @@ import classnames from 'classnames';
 
 import './menuItem.scss';
 
-import menuItemConfig from "./menuItem.config";
 
 export default function MenuItem(props) {
-    const { item } = props;
-    const config = menuItemConfig[item];
-    const { number, img, text } = config;
+    const { number, img, text } = props.item;
+    const { position } = props;
 
-
-    function renderText(text) {
-        return text.split('').map((letter, index) => {
-                return ( <div className='menuItem__letter' key={index}>
-                    <div className='menuItem__letter_inner'>
-                    {letter}
-                    </div>
-                </div>)
-            }
+    const renderText = (text) => {
+        return text.split('').map((letter, index) => (
+            <div className='menuItem__letter' key={index}>
+                <div className='menuItem__letter_inner'>
+                {letter}
+                </div>
+            </div>
+            )
         )
     }
 
     return (
         <div className='container'>
-            <div className ='menuItem'>
+            <div className ={classnames('menuItem', position)}>
                 <div className='menuItem__number'>
                     {number}
                 </div>
-                <img className={classnames({'menuItem__img': true, 'menuItem__img_3': (item===3)})} src={img} alt='menu icon'/>
+                <img className='menuItem__img' src={img} alt={text}/>
                 <span className='menuItem__text'>
                     {renderText(text)}
                 </span>
--- src/menuConfig.js
+++ src/menuConfig.js
@@ -1,28 +1,30 @@
-import CatImage from '../../assets/images/cat.png';
-import DogImage from '../../assets/images/dog.png';
-import FestivalImage from '../../assets/images/festival.png';
+import CatImage from './assets/images/cat.png';
+import DogImage from './assets/images/dog.png';
+import FestivalImage from './assets/images/festival.png';
 
-const menuItemConfig = {
-    1: {
+export default [
+    {
+        id: 1,
         number: '01',
         img: CatImage,
         text: 'one'
     },
-    2: {
+    {
+        id: 2,
         number: '02',
         img: DogImage,
         text: 'two'
     },
-    3: {
+    {
+        id: 3,
         number: '03',
         img: FestivalImage,
         text: 'three'
     },
-    4: {
+    {
+        id: 4,
         number: '03',
         img: FestivalImage,
         text: 'four'
     },
-};
-
-export default menuItemConfig;
+];
